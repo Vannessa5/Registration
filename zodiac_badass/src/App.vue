6ddaedbce4ebaac51c80f439f155671e1eb16b3d
Vannessa5 <vanematenga@gmail.com>
2020-11-01 20:46:12 +0200

    VueJS

---
+++ zodiac_badass/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Reg logo" src="./assets/logo.png">
+    <Home msg="Home Page!"/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ zodiac_badass/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="app">
+        <form>
+            <h3>Sign Up</h3>
+
+            <div class="form-group">
+                <label>Full Name</label>
+                <input type="text" id="name" class="form-control form-control-lg"/>
+            </div>
+
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" id ="email" class="form-control form-control-lg" />
+            </div>
+
+            <div class="form-group">
+                <label>Phone Number</label>
+                <input type="number" id="phoneNumber" class="form-control form-control-lg" />
+            </div>
+
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
+
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
